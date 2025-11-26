2eb684554acc36bdc108aea889ddd643f244bd3a
Samata-Statistics <smd7237@psu.edu>
2025-11-26 17:19:27 -0500

    Update runtests.jl

---
+++ test/runtests.jl
@@ -0,0 +1,10 @@
+using ExamplePkg
+
+add(3, 5)           # 8
+mul(2, 7)           # 14
+div_safe(10, 2)     # 5.0
+pow(2, 3)           # 8
+
+x = [1.0, 2.0, 3.0, 4.0]
+mean_vector(x)      # 2.5
+std_vector(x)       # ≈ 1.29099
